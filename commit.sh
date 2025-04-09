b4c3a5ba2d7d13752b87d972caecbcf535c428f1
asiamserrano <asiamserrano@gmail.com>
2025-04-09 19:43:59 -0400

    updates

--- commit.sh
+++ commit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+git add .gitignore
+git add commit.sh
+git add autosave_ios/autosave_ios.xcodeproj/project.pbxproj 
 git add \*.swift
 git commit -m "${1:-updates}"  
 git push
+git status
